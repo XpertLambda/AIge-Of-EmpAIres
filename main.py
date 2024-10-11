42ed192242ca394671cffcc3c82203862f635c12
neptune2716 <cyril.hup2716@gmail.com>
2024-10-11 15:57:30 +0200

    Map fonctionnel sans textures

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
-from Controller.init_map import init_pygame, load_textures, game_loop
+# main.py
+
+from Controller.init_map import init_pygame, game_loop
 from Models.Map import GameMap
 
 def main():
-    screen = init_pygame()
-    textures = load_textures()
+    # Déballer les retours de init_pygame
+    screen, screen_width, screen_height = init_pygame()
+    
+    # Initialiser la carte de jeu
     game_map = GameMap()
-    game_map.print_map()
-    game_loop(screen, game_map, textures)
+    game_map.print_map()  # Affiche la carte dans la console pour le débogage
+    
+    # Lancer la boucle de jeu avec tous les arguments requis
+    game_loop(screen, game_map, screen_width, screen_height)
 
 if __name__ == "__main__":
     main()
